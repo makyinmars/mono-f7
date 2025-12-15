685c56283802763f5b07bdf39e8ba965c120d143
Franklin Jara <fjara@cherryhillprograms.com>
2025-12-15 15:19:14 -0500

    clean up

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,5 +1,29 @@
+// import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+// import viteReact from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import tsConfigPaths from "vite-tsconfig-paths";
+//
+// export default defineConfig({
+//   server: {
+//     port: 3001,
+//   },
+//   plugins: [
+//     tsConfigPaths({
+//       projects: ["./tsconfig.json"],
+//     }),
+//     tanstackStart({
+//       target: "bun",
+//       customViteReactPlugin: true,
+//     }),
+//     viteReact(),
+//   ],
+// });
+
+// import { lingui } from "@lingui/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -7,14 +31,30 @@ export default defineConfig({
   server: {
     port: 3001,
   },
+  build: {
+    target: "esnext",
+  },
   plugins: [
+    // lingui(),
+    nitro(),
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
-    tanstackStart({
-      target: "bun",
-      customViteReactPlugin: true,
+    tailwindcss(),
+    tanstackStart(),
+    viteReact({
+      // babel: {
+      //   plugins: ["@lingui/babel-plugin-lingui-macro"],
+      // },
     }),
-    viteReact(),
   ],
+  ssr: {
+    noExternal: ["ms"],
+  },
+  nitro: {
+    preset: "aws-lambda",
+    awsLambda: {
+      streaming: false,
+    },
+  },
 });
--- apps/store/vite.config.ts
+++ apps/store/vite.config.ts
@@ -1,5 +1,29 @@
+// import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+// import viteReact from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import tsConfigPaths from "vite-tsconfig-paths";
+//
+// export default defineConfig({
+//   server: {
+//     port: 3001,
+//   },
+//   plugins: [
+//     tsConfigPaths({
+//       projects: ["./tsconfig.json"],
+//     }),
+//     tanstackStart({
+//       target: "bun",
+//       customViteReactPlugin: true,
+//     }),
+//     viteReact(),
+//   ],
+// });
+
+// import { lingui } from "@lingui/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -7,11 +31,30 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    target: "esnext",
+  },
   plugins: [
+    // lingui(),
+    nitro(),
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
+    tailwindcss(),
     tanstackStart(),
-    viteReact(),
+    viteReact({
+      // babel: {
+      //   plugins: ["@lingui/babel-plugin-lingui-macro"],
+      // },
+    }),
   ],
+  ssr: {
+    noExternal: ["ms"],
+  },
+  nitro: {
+    preset: "aws-lambda",
+    awsLambda: {
+      streaming: false,
+    },
+  },
 });
